b57a617024b7a2fc22f5a8c37bf579c6e77c537a
Christopher Cervantes <>
2023-04-10 20:04:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from random import*
+from game import*
+
+intro = input(print("Would you like to play Higher or Lower: Video Game Sales? Y/N "))
+if intro == 'Y' or 'y':
+  game
+elif intro == 'N' or 'n':
+  print("Ok, See You Later! ")
