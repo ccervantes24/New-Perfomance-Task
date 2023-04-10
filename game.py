ae4563ca907143560b02f13c089b5084a2307ed5
Christopher Cervantes <113720575+ccervantes24@users.noreply.github.com>
2023-04-10 20:19:40 +0000

    Beginning of games. Need to work on the 'No' option.

--- game.py
+++
@@ -1,3 +0,0 @@
-# games = ["Tetris (1984)", "Minecraft (2009)", "GTA V (2013))", "Wii Sports (2006)", "PUBG (2017)", "Mario Kart 8/Mario Kart 8 Deluxe (2014)", "Super Mario Bros. (1985)", "Pokémon Generation 1 (1996)", "Pokémon Generation 1 (1996)"]
-
-def game
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from random import*
-from game import*
+import random
+
+games = ["Tetris (1984)", "Minecraft (2009)", "GTA V (2013)", "Wii Sports (2006)", "PUBG (2017)", "Mario Kart 8/Mario Kart 8 Deluxe (2014)", "Super Mario Bros. (1985)", "Pokémon Generation 1 (1996)", "Pokémon Generation 1 (1996)"] 
 
 intro = input(print("Would you like to play Higher or Lower: Video Game Sales? Y/N "))
 if intro == 'Y' or 'y':
-  game
-elif intro == 'N' or 'n':
-  print("Ok, See You Later! ")
+    one = input(f'{random.choice(games)} or {random.choice(games)}')
+else:
+    print("Ok, See You Later! ")
