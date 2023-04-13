1f41e1d21bf0eb89a5df98f43ba0c24e62ebccd4
Christopher Cervantes <113720575+ccervantes24@users.noreply.github.com>
2023-04-13 20:17:17 +0000

    no more random

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import random
 
-games = ["Tetris (1984)", "Minecraft (2009)", "GTA V (2013)", "Wii Sports (2006)", "PUBG (2017)", "Mario Kart 8/Mario Kart 8 Deluxe (2014)", "Super Mario Bros. (1985)", "Pokémon Generation 1 (1996)", "Pokémon Generation 1 (1996)"] 
+games = ["Tetris", "Minecraft ", "GTA V ", "Wii Sports ", "PUBG ", "Mario Kart 8/Mario Kart 8 Deluxe","Overwatch ", "Super Mario Bros ", "Pokémon Generation 1", "Red Dead Redemption 2"] 
 
-intro = input(print("Would you like to play Higher or Lower: Video Game Sales? Y/N "))
-if intro == 'Y' or 'y':
-    one = input(f'{random.choice(games)} or {random.choice(games)}')
-else:
-    print("Ok, See You Later! ")
+values = [games[0] == 10, games[1] == 9, games[2] == 8, games[3] == 7, games[4] == 6, games[5] == 5, games[6] == 4, games[7] == 3, games[8] == 2, games[9] == 1 ]
+intro = input("Would you like to play Higher or Lower: Video Game Sales? Y/N ").upper()
+if intro == "N":
+  print("Okay, thank you for your time!")
+elif intro == "Y":
+  one = input(f'1. {random.choice(games)}(1) or {random.choice(games)}(2)')
+  
+    
